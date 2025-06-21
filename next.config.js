483de25a8499e154476c310efdb1746e5e134473
Your Name <your.email@example.com>
2025-06-21 12:27:18 -0400

    fix: configure edge runtime and proper Netlify edge functions

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   images: {
     unoptimized: true
+  },
+  experimental: {
+    runtime: 'edge'
   }
 }
 
