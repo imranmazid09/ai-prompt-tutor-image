075d99f7fad49b97560da560cc3cc8cbc5bc4e1a
Your Name <your.email@example.com>
2025-06-21 12:50:51 -0400

    fix: simplify deployment configuration and remove middleware

--- next.config.js
+++ next.config.js
@@ -1,16 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   images: {
     unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
-  },
-  poweredByHeader: false
+    domains: ['**']
+  }
 }
 
 module.exports = nextConfig
